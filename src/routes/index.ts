131daf5572ea2231df5fd6f1985f5d803093dc3d
dsiguero <26867647+dsiguero@users.noreply.github.com>
2024-12-13 12:52:28 +0100

    feat: redirect user to dashboard if authenticated

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,15 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
+import { isAuthenticated } from '../auth/auth';
 import { Login } from '../pages/Login';
 
 export const Route = createFileRoute('/')({
+  beforeLoad: ({ location }) => {
+    // If the user is authenticated, there is no need to show them the login page.
+    if (isAuthenticated()) {
+      return redirect({
+        to: '/dashboard'
+      });
+    }
+  },
   component: Login
 });
