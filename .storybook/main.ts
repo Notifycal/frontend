ba5c6bf2ca65c29c267eeba8aebe4c9f75b95d37
Daniel Siguero <26867647+dsiguero@users.noreply.github.com>
2024-12-18 00:38:34 +0100

    feat: re setup storybook (#11)
    Ready to review @sergiojoker11!
    
    It all started with the thought of writing a small LanguagePicker
    component, leveraging Storybook. But then the Storybook setup of our
    friend Ricardo wasn't completely right (surpriseee!).
    
    Then Tailwind started fighting Mantine, but that's hopefully fixed by
    tweaking the imports order in CSS.
    
    ![pls](https://i.imgflip.com/3jf0ic.jpg)
    
    But that's fixed now (afaict).
    
    You should be able to run `npm run storybook` if you want to have a
    look. Check Page > Logged In, then jump to the Interactions tab at the
    bottom.
    
    BTW, the `sampleStories` folder can go at any time, just thought it
    wouldn't hurt to have it there for now.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,13 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
     '@storybook/addon-interactions',
-    '@storybook/addon-styling',
-    {
-      name: '@storybook/addon-styling',
-      options: {}
-    },
-    {
-      name: '@storybook/addon-styling',
-      options: {}
-    }
+    '@storybook/addon-themes'
   ],
   framework: {
     name: '@storybook/react-vite',
