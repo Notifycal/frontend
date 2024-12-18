bff273eaa0025283eee3f8621b0ff5ccd007e2be
dsiguero <26867647+dsiguero@users.noreply.github.com>
2024-12-18 10:33:28 +0100

    update AuthProvider

--- src/hooks/AuthProvider.tsx
+++ src/hooks/AuthProvider.tsx
@@ -1,33 +1,51 @@
 import type { TokenResponse } from '@react-oauth/google';
-import { createContext, useCallback, useContext, useState } from 'react';
+import { createContext, useCallback, useContext, useState, type ReactNode } from 'react';
 import type { FunctionComponent } from '../common/types';
 
-const AuthContext = createContext({
-  tokenResponse: null,
-  login: () => {},
-});
+export interface AuthContext {
+  isAuthenticated: boolean;
+  tokenResponse: TokenResponse;
+  login: () => void;
+}
 
-interface AuthProviderProps {
-  children: React.ReactElement;
-};
+const AuthContext = createContext<AuthContext | null>(null);
 
-const AuthProvider = ({ children }: AuthProviderProps): FunctionComponent => {
-  const [ tokenResponse, setTokenResponse ] = useState<TokenResponse | null>(null);
+const key = 'isAuthenticated';
+
+function getAuthenticationStatus(): boolean {
+  return localStorage.getItem(key) == 'true';
+}
+
+function setAuthenticationStatus(authStatus: boolean): void {
+  if (authStatus) {
+    localStorage.setItem(key, `${authStatus}`);
+  } else {
+    localStorage.removeItem(key);
+  }
+}
+
+const AuthProvider = ({ children }: {children: ReactNode}): FunctionComponent => {
+  const [tokenResponse, setTokenResponse] = useState<TokenResponse | null>(null);
+  const isAuthenticated = getAuthenticationStatus();
+
 
   // eslint-disable-next-line unicorn/prevent-abbreviations
   const login = useCallback((tokenRes: TokenResponse | null) => {
     if (tokenRes !== null) {
+      setAuthenticationStatus(true);
       setTokenResponse(tokenRes);
     }
   }, []);
 
-  return (
-    <AuthContext.Provider value={{tokenResponse, login}}>{children}</AuthContext.Provider>
-  );
+  return <AuthContext.Provider value={{ isAuthenticated, tokenResponse, login }}>{children}</AuthContext.Provider>;
 };
 
 export default AuthProvider;
 
-export const useAuth = () => {
-  return useContext(AuthContext);
+export const useAuth = (): AuthContext | Error  => {
+  const context = useContext(AuthContext);
+  if (! context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
 };
