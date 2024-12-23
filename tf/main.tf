36512489c5ff4ec8c81758742f58b2c44de3995f
dsiguero <26867647+dsiguero@users.noreply.github.com>
2024-12-23 12:39:07 +0100

    use updated module

--- tf/main.tf
+++ tf/main.tf
@@ -1,31 +1,11 @@
-data "cloudflare_zone" "main" {
-  name = var.base_domain
-}
-
 module "static_landing" {
-  source = "git@github.com:Notifycal/tofu-module-static-website.git?ref=v1.0.1"
+  source = "git@github.com:Notifycal/tofu-module-static-website.git?ref=v1.1.0"
 
-  bucket_name = local.domain
+  base_domain   = var.base_domain
+  domain_prefix = var.domain_prefix
 
-  enable_s3_public_access = false
-  enable_www_redirect     = var.enable_www_redirect
-}
+  enable_www_redirect   = var.enable_www_redirect
+  redirect_base_domains = var.redirect_base_domains
 
-resource "cloudflare_record" "main" {
-  zone_id = data.cloudflare_zone.main.id
-  # @ is how Cloudflare calls the naked domain
-  name    = local.main_dns_record_name
-  value   = module.static_landing.site_urls.main
-  type    = "CNAME"
-  proxied = true
-}
-
-resource "cloudflare_record" "www_redirect" {
-  count = var.enable_www_redirect ? 1 : 0
-
-  zone_id = data.cloudflare_zone.main.id
-  name    = local.redirect_dns_record_name
-  value   = module.static_landing.site_urls.redirect
-  type    = "CNAME"
-  proxied = true
+  cloudflare_private_site_auth_enabled = !var.is_public
 }
