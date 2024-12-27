345a81f4c63164dd2e5c59e412f6ceb338977364
dsiguero <26867647+dsiguero@users.noreply.github.com>
2024-12-27 01:37:38 +0100

    oops

--- tf/main.tf
+++ tf/main.tf
@@ -1,6 +1,5 @@
 module "frontend" {
-  source = "/Users/dan/dev/personal/notifycal/modules/tofu-module-static-website"
-  # source = "git@github.com:Notifycal/tofu-module-static-website.git?ref=v1.1.1"
+  source = "git@github.com:Notifycal/tofu-module-static-website.git?ref=v1.1.1"
 
   base_domain   = var.base_domain
   domain_prefix = var.domain_prefix
