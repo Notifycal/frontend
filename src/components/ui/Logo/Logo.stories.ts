1aab433e433495e8c069651bb1d125ff35151cae
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-01-02 12:28:19 +0100

    amend story

--- src/components/ui/Logo/Logo.stories.ts
+++ src/components/ui/Logo/Logo.stories.ts
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
 
 import Logo from './Logo';
 
@@ -15,10 +14,8 @@ const meta = {
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    displayFlagOnly: { control: 'boolean' }
+    classNames: { control: 'text' }
   },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onLanguageSelected: fn() }
 } satisfies Meta<typeof Logo>;
 
 export default meta;
@@ -26,7 +23,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Main: Story = {
   args: {
-    displayFlagOnly: false
+    classNames: 'mx-auto w-auto'
   }
 };
 
