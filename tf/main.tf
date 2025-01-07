8ed4e0abe77bdf3955cb4d430ecbbdf85bfbddce
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-01-07 23:01:07 +0100

    feat: add cloudflare_config parameter to determine whether or not cloudflare stuff is instantiated. If not set, public acccess to S3 bucket is setup. The goal is to let local env use this module

--- tf/main.tf
+++ tf/main.tf
@@ -1,15 +1,10 @@
 module "frontend" {
-  source = "git@github.com:Notifycal/tofu-module-static-website.git?ref=v1.1.1"
+  source = "git@github.com:Notifycal/tofu-module-static-website.git?ref=v2.0.0"
 
   base_domain   = var.base_domain
   domain_prefix = var.domain_prefix
 
   enable_www_redirect = var.enable_www_redirect
 
-  cloudflare_config = !var.is_local_env ? {
-    account_name = "notifycal.com"
-    private_site_auth = !var.is_public ? {
-      idp_name = "Github"
-    } : null
-  } : null
+  cloudflare_config = var.cloudflare_config
 }
