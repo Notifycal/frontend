3c103fff57fdc47b1a9a138e6c9e0f6ea5ac1fee
SeRGiOJoKeR11 <sergio.anger@gmail.com>
2025-01-15 00:01:38 +0100

    feat: add config so app can interact with localstack too (#93)

--- config/config.local.js
+++ config/config.local.js
@@ -1,5 +1,6 @@
 window.globalConfig = {
   GOOGLE_CLIENT_ID: '658640078137-omuaokg6rcajv50879674moielbpvljl.apps.googleusercontent.com',
   BACKEND_BASE_URL: 'http://localhost:8080',
+  // BACKEND_BASE_URL: 'http://localhost:4566/_aws/execute-api/my-api/local', // Toggle with above line when you are running against Localstack
   STATIC_LANDING_URL: 'http://localhost:4321'
 };
