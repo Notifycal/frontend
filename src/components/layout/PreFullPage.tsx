df8a4a6c611286d4c8ed28503b9db4b28b005f69
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-01-24 20:05:32 +0100

    drop comment

--- src/components/layout/PreFullPage.tsx
+++ src/components/layout/PreFullPage.tsx
@@ -3,9 +3,5 @@ import type { ReactNode } from 'react';
 import type { FunctionComponent } from '@common/types';
 
 export default function PreFullPage({ children }: { children: ReactNode }): FunctionComponent {
-  return (
-    // TODO: review
-    // <div className="flex h-screen flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100"></div>
-    <div className="flex h-screen flex-1 flex-col justify-center bg-gray-100">{children}</div>
-  );
+  return <div className="flex h-screen flex-1 flex-col justify-center bg-gray-100">{children}</div>;
 }
