43492babe6b6e992346d76a574fca6e1dac9c04e
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-05-08 14:18:14 +0200

    add backend back to i18n and type for keys

--- src/i18next.d.ts
+++ src/i18next.d.ts
@@ -1,9 +1,13 @@
 import 'i18next';
 
 import type { defaultNS } from '@common/i18n';
+import type { NestedKeyOf } from '@common/types';
 
 import type onboarding from '@assets/locales/es/onboarding.json';
 import type translations from '@assets/locales/es/translations.json';
+
+type OnboardingKeys = NestedKeyOf<typeof onboarding>;
+type TranslationKeys = NestedKeyOf<typeof translations>;
 
 declare module 'i18next' {
   interface CustomTypeOptions {
@@ -13,4 +17,6 @@ declare module 'i18next' {
       translations: typeof translations;
     };
   }
+
+  type I18nKey = OnboardingKeys | TranslationKeys;
 }
