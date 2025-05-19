c49ac79cee948adfdcb6e576ea3788f67ba4d942
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-05-19 20:44:39 +0200

    drop unused i18n JSON key type in favour of the one in the library

--- src/common/types.ts
+++ src/common/types.ts
@@ -5,15 +5,3 @@ export type KebabCase<S> = S extends `${infer C}${infer T}`
     ? `${Uncapitalize<C>}${KebabCase<T>}`
     : `${Uncapitalize<C>}-${KebabCase<T>}`
   : S;
-
-type Previous = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // Helper: Depth counter array
-// Recursive key path type with depth limit. Otherwise: Type instantiation is excessively deep and possibly infinite.
-export type NestedKeyOf<T, D extends number = 10> = [D] extends [never]
-  ? never
-  : T extends object
-    ? {
-        [K in Extract<keyof T, string>]: T[K] extends object
-          ? `${K}` | `${K}.${NestedKeyOf<T[K], Previous[D]>}`
-          : `${K}`;
-      }[Extract<keyof T, string>]
-    : never;
