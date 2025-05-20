ec2552f06c07791287a1ecbf52c7c62ff7b77d46
SeRGiOJoKeR11 <sergio.anger@gmail.com>
2025-05-20 21:31:43 +0200

    fix: welcome navigation bug (#253)

--- src/routes/_auth/onboarding/index.tsx
+++ src/routes/_auth/onboarding/index.tsx
@@ -7,6 +7,9 @@ export const Route = createFileRoute('/_auth/onboarding/')({
     const { completedSteps } = useOnboardingStore.getState();
 
     const firstIncompleteIndex = getFirstIncompleteStepIndex(completedSteps) || 0;
+    if (firstIncompleteIndex === 0) {
+      throw redirect({ to: '/onboarding/welcome' });
+    }
     const path = getStepByIndex(firstIncompleteIndex)?.path || '';
     throw redirect({ to: '/onboarding/$step', params: { step: path } });
   }
