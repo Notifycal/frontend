abf94b746a8c7444ec7d9f71fa7f60742a102962
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-06-03 22:20:35 +0200

    chore: major Storybook upgrade

--- src/components/ui/PhoneInput/PhoneInput.stories.ts
+++ src/components/ui/PhoneInput/PhoneInput.stories.ts
@@ -1,5 +1,5 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { fn } from 'storybook/test';
 
 import type { PhoneNumber } from '@notifycal/shared/types';
 import PhoneInput from './PhoneInput';
