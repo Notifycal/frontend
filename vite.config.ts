629dd8fb552c07fd9485eda5f49128d295784223
Daniel Siguero <26867647+dsiguero@users.noreply.github.com>
2025-07-01 13:07:41 +0200

    local: fix tabler-icons 5k chunks (#329)

--- vite.config.ts
+++ vite.config.ts
@@ -8,32 +8,43 @@ import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    TanStackRouterVite(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: normalizePath(path.resolve('./src/assets/locales')),
-          dest: normalizePath(path.resolve('./dist'))
-        }
-      ]
-    }),
-    tsconfigPaths()
-  ],
-  server: {
-    host: true,
-    strictPort: true,
-    port: 5173
-  },
-  preview: {
-    port: 5173
-  },
-  test: {
-    environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts'],
-    css: true,
-    passWithNoTests: true
-  }
+export default defineConfig(({ command, mode }) => {
+  const isLocalDev = mode !== 'production' && command === 'serve';
+
+  const resolveDev = {
+    alias: {
+      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs'
+    }
+  };
+
+  return {
+    plugins: [
+      react(),
+      TanStackRouterVite(),
+      viteStaticCopy({
+        targets: [
+          {
+            src: normalizePath(path.resolve('./src/assets/locales')),
+            dest: normalizePath(path.resolve('./dist'))
+          }
+        ]
+      }),
+      tsconfigPaths()
+    ],
+    resolve: isLocalDev ? resolveDev : {},
+    server: {
+      host: true,
+      strictPort: true,
+      port: 5173
+    },
+    preview: {
+      port: 5173
+    },
+    test: {
+      environment: 'jsdom',
+      setupFiles: ['./vitest.setup.ts'],
+      css: true,
+      passWithNoTests: true
+    }
+  };
 });
