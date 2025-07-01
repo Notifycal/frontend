6ba899ea2b36db56c66f9be69d101dba5daf8095
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-07-01 12:24:39 +0200

    put TIER_INFO back

--- config/config.local.js
+++ config/config.local.js
@@ -1,5 +1,6 @@
 window.globalConfig = {
   GOOGLE_CLIENT_ID: '658640078137-omuaokg6rcajv50879674moielbpvljl.apps.googleusercontent.com',
   BACKEND_BASE_URL: 'http://localhost:8080',
-  STATIC_LANDING_URL: 'http://localhost:4321'
+  STATIC_LANDING_URL: 'http://localhost:4321',
+  TIER_INFO: '{"best":{"name":"Best Plan","price_eur":60,"price_id":"best_price_id_mock","product_id":"best_product_id_mock", "number_of_reminders": 100},"better":{"name":"Better Plan","price_eur":25,"price_id":"better_price_id_mock","product_id":"better_product_id_mock", "number_of_reminders": 50},"good":{"name":"Good Plan","price_eur":10,"price_id":"good_price_id_mock","product_id":"good_product_id_mock", "number_of_reminders": 20}}'
 };
