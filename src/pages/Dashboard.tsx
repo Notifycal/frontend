ec55e491473074c4405d5abf0a7e274831a381e9
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-07-05 01:29:28 +0200

    feat: display whole user info in dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,20 +3,47 @@ import { useQuery } from '@tanstack/react-query';
 import { getUserProfile } from '@api/userProfile';
 
 import type { FunctionComponent } from '@common/types';
+import type { JSX } from 'react';
+import { isObject } from 'radashi';
+
+type DisplayProps = {
+  data: unknown;
+  depth?: number;
+};
+
+function KeyValueRecursive({ data, depth = 0 }: DisplayProps): JSX.Element {
+  if (Array.isArray(data)) {
+    return (
+      <div style={{ marginLeft: depth * 16 }}>
+        {data.map((item, index) => (
+          <div key={index}>
+            <strong>[{index}]:</strong> <KeyValueRecursive data={item} depth={depth + 1} />
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  if (isObject(data)) {
+    return (
+      <div style={{ marginLeft: depth * 16 }}>
+        {Object.entries(data).map(([key, value]: [string, unknown]) => (
+          <div key={key} className="mb-1">
+            <strong>{key}:</strong> <KeyValueRecursive data={value} depth={depth + 1} />
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  return <span>{String(data)}</span>;
+}
 
 export const Dashboard = (): FunctionComponent => {
-  // const queryClient = useQueryClient();
   const query = useQuery({
     queryKey: ['user-profile'],
     queryFn: getUserProfile
   });
 
-  return (
-    <>
-      <span>
-        Username:
-        {!!query.data && <span> {query.data.userId}</span>}
-      </span>
-    </>
-  );
+  return <div>{!!query.data && <KeyValueRecursive data={query.data} />}</div>;
 };
