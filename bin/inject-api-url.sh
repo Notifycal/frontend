8a04911d829b4eaa3b52340c3466adcc07d3c0dd
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-07-04 22:53:12 +0200

    local: handle failure scenarios for inject-api-url script

--- bin/inject-api-url.sh
+++ bin/inject-api-url.sh
@@ -20,11 +20,21 @@ REGION="${AWS_REGION:-$(aws configure get region)}"
 CONFIG_FILE="config/config.local.js"
 
 if [ -z "$REGION" ]; then
-  echo "AWS region not configured. Set AWS_REGION or run 'aws configure'."
+  echo "❌ AWS region not configured. Set AWS_REGION or run 'aws configure'."
   exit 1
 fi
 
-REST_API_ID=$(aws apigateway get-rest-apis | jq -rc --arg env "${ENV_NAME}" '.items[] | select(.tags.Environment == $env) | .id')
+if ! aws sts get-caller-identity 2>/dev/null; then
+  echo "❌ AWS credentials or region not configured."
+  exit 1
+fi
+
+if ! REST_API_ID=$(aws apigateway get-rest-apis | jq -rce --arg env "${ENV_NAME}" '.items[] | select(.tags.Environment == $env) | .id'); then
+  echo "❌ No REST API found for environment: '${ENV_NAME}'." >&2
+  echo "Exiting." >&2
+  exit 1;
+fi
+
 URL="https://${REST_API_ID}.execute-api.${REGION}.amazonaws.com/${STAGE_NAME}"
 
 echo "Updating BACKEND_BASE_URL to: $URL"
