5da7aaced956654288aef398a1ec8d065968365c
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-07-23 11:35:54 +0200

    simplify

--- src/constants/tiers.tsx
+++ src/constants/tiers.tsx
@@ -1,11 +1,2 @@
-import type { TierId } from '@notifycal/shared/types';
-
 type ExpectedTierOrder = readonly ['good', 'better', 'best'];
-
-type IsExactOrder<T extends ReadonlyArray<TierId>> = T extends ExpectedTierOrder
-  ? ExpectedTierOrder extends T
-    ? T
-    : never
-  : never;
-
-export const tierOrder: IsExactOrder<ExpectedTierOrder> = ['good', 'better', 'best'];
+export const tierOrder: ExpectedTierOrder = ['good', 'better', 'best'];
