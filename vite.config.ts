781fa80306bbd512c33f9b4067284fb2fd9bd14c
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-08-02 20:55:45 +0200

    build/bundle-sizes

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react-swc';
-import path from 'node:path';
 import fs from 'node:fs';
+import path from 'node:path';
 import { normalizePath } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 import { defineConfig } from 'vitest/config';
 
 import tsconfigPaths from 'vite-tsconfig-paths';
+const maxBundleChunkSizeInBytes = 1.3 * 1024 * 1024; //MB
+const maxTotalBundleSizeInBytes = 1.8 * 1024 * 1024; //MB
 
 const handleServiceConfigPlugin = (): import('vite').Plugin => {
   const pluginName = 'service-config-handler';
@@ -39,6 +41,43 @@ const handleServiceConfigPlugin = (): import('vite').Plugin => {
   };
 };
 
+const handleBundleSizePlugin = (): import('vite').Plugin => {
+  const pluginName = 'bundle-size-limit';
+  return {
+    name: pluginName,
+    generateBundle(_options, bundle) {
+      console.log(`[${pluginName}] Checking bundle size limits...`);
+      let totalSize = 0;
+      for (const [fileName, chunk] of Object.entries(bundle)) {
+        let size = 0;
+
+        if (chunk.type === 'chunk' && chunk.code) {
+          size = Buffer.byteLength(chunk.code, 'utf8');
+          if (size > maxBundleChunkSizeInBytes) {
+            throw new Error(
+              `Chunk ${fileName} ${bytestoMB(size)}MB exceeds the chunk size limit of ${bytestoMB(maxBundleChunkSizeInBytes)}MB`
+            );
+          }
+        } else if (chunk.type === 'asset' && chunk.source) {
+          size = Buffer.isBuffer(chunk.source) ? chunk.source.length : Buffer.byteLength(chunk.source, 'utf8');
+        }
+        totalSize += size;
+      }
+      const totalSizeInMB = bytestoMB(totalSize);
+      console.log(`Total bundle size: ${totalSizeInMB}MB`);
+      if (totalSize > maxTotalBundleSizeInBytes) {
+        throw new Error(
+          `Total bundle size ${totalSizeInMB}MB exceeds the total bundle size limit of ${bytestoMB(maxTotalBundleSizeInBytes)}MB`
+        );
+      }
+    }
+  };
+};
+
+export function bytestoMB(numberOfBytes: number): string {
+  return (numberOfBytes / 1024 / 1024).toFixed(2);
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -57,6 +96,13 @@ export default defineConfig({
     tsconfigPaths(),
     handleServiceConfigPlugin()
   ],
+  build: {
+    reportCompressedSize: false,
+    chunkSizeWarningLimit: (maxBundleChunkSizeInBytes / 1024) * 0.9, // Expressed in KB. The budget is 90% of the limit.
+    rollupOptions: {
+      plugins: [handleBundleSizePlugin()]
+    }
+  },
   server: {
     host: true,
     strictPort: true,
