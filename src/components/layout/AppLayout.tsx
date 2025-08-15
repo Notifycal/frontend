baa22a191179af74c5cc3e513a8870a95515a50f
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-08-15 13:49:34 +0200

    remove main container border

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -40,7 +40,7 @@ const AppLayout: FC<AppLayoutProps> = ({ children }) => {
       </div>
       <main className={clsx({ '-mt-32': useFancyHeader })}>
         <div className="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
-          <div className="rounded-lg bg-white px-5 py-6 shadow-sm sm:px-6">{children}</div>
+          <div className="rounded-lg px-5 py-6 sm:px-6">{children}</div>
         </div>
       </main>
 
