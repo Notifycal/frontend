12cacd970ba3aea228b25c8511d0d5e7e8c6c173
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-08-18 21:38:35 +0200

    buiild service config plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { bundleSizePlugin } from '@notifycal/shared/utils';
+import { bundleSizePlugin, ourServiceConfigPlugin } from '@notifycal/shared/utils';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react-swc';
-import fs from 'node:fs';
 import path from 'node:path';
 import { normalizePath } from 'vite';
 import { plugin as markdown, Mode } from 'vite-plugin-markdown';
@@ -11,37 +10,6 @@ import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 const maxBundleChunkSizeInBytes = 1.4 * 1024 * 1024; //MB
 const maxTotalBundleSizeInBytes = 2 * 1024 * 1024; //MB
-
-const handleServiceConfigPlugin = (): import('vite').Plugin => {
-  const pluginName = 'service-config-handler';
-  const configLocalPath = path.resolve(__dirname, 'config/config.local.js');
-  const configSkelPath = path.resolve(__dirname, 'config/config.skel.js');
-
-  return {
-    name: pluginName,
-    configureServer: (server) => {
-      // Only runs for the dev server
-      console.log(`[${pluginName}] Serving local config.js...`);
-      server.middlewares.use('/config.js', (req, res) => {
-        const content = fs.readFileSync(configLocalPath, 'utf-8');
-        res.setHeader('Content-Type', 'application/javascript');
-        res.statusCode = 200;
-        res.end(content);
-      });
-    },
-    generateBundle() {
-      // Only runs for the prod build
-      console.log(`[${pluginName}] Bundling config.skel.js...`);
-      const content = fs.readFileSync(configSkelPath, 'utf-8');
-      this.emitFile({
-        type: 'asset',
-        fileName: 'config.skel.js',
-        source: content
-      });
-      console.log(`[${pluginName}] Emitted config.skel.js to bundle.`);
-    }
-  };
-};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -60,7 +28,7 @@ export default defineConfig({
       ]
     }),
     tsconfigPaths(),
-    handleServiceConfigPlugin()
+    ourServiceConfigPlugin(__dirname)
   ],
   build: {
     reportCompressedSize: false,
