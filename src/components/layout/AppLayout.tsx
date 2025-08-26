c173063f1590572591cfe19bd86be7135fa800fc
Daniel Siguero <26867647+dsiguero@users.noreply.github.com>
2025-08-26 17:57:58 +0200

    feat: i18n page title with index.html fallback (#596)

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -4,12 +4,15 @@ import type { FC } from 'react';
 import Footer from '@components/ui/Footer/Footer';
 import { useAuth } from '@providers/AuthProvider';
 import { TwSizeIndicator } from '@components/utils/development-tools/TwSizeIndicator';
+import { useTranslation } from 'react-i18next';
 
 const AppLayout: FC = () => {
   const { isAuthenticated } = useAuth();
+  const { t } = useTranslation();
 
   return (
     <>
+      <title>{t('pageTitle')}</title>
       <TwSizeIndicator />
       <div className="min-h-[100dvh] flex flex-col">
         <div className="flex-grow bg-neutral-50 flex flex-col">
