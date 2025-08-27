9649edd1e4a0749a1c795301abd6c70bcbff2c78
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-08-27 22:52:21 +0200

    fix: layout, i18n, close on drawer item click, replace /dashboard with /billing, etc

--- src/components/ui/Navigation/Navigation.tsx
+++ src/components/ui/Navigation/Navigation.tsx
@@ -1,56 +1,36 @@
-import { Burger, Drawer, Menu, rem } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
+import { Menu } from '@mantine/core';
 import NotifycalIsologo from '@notifycal/shared/assets/logos/notifycal-isologo.svg?react';
 import type { UserModel } from '@our-types/UserModel';
 import { useAuth } from '@providers/AuthProvider';
-import { IconLogout2 } from '@tabler/icons-react';
-import { Link } from '@tanstack/react-router';
+import { IconLogout2, IconUserFilled } from '@tabler/icons-react';
 import type { JSX } from 'react';
-
-const navigation = [
-  { name: 'Dashboard', href: '/dashboard' },
-  { name: 'Template', href: '/template' },
-  { name: 'Billing', href: '/billing' },
-  { name: 'Feedback', href: '/feedback' }
-];
-
-const userNavigation = [
-  { name: 'Your Profile', href: '#' },
-  { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '#' }
-];
-
-interface NavigationMenuProps {
-  className: string;
-  activeClassName: string;
-}
-
-const NavigationMenu = ({ className, activeClassName }: NavigationMenuProps): JSX.Element => (
-  <>
-    {navigation.map((item) => (
-      <Link key={item.href} activeProps={{ className: activeClassName }} className={className} to={item.href}>
-        {item.name}
-      </Link>
-    ))}
-  </>
-);
+import NavigationMenu from '../NavigationMenu/NavigationMenu';
+import NavigationDrawer from '../NavigationDrawer/NavigationDrawer';
+import { useTranslation } from 'react-i18next';
 
 interface UserProps {
   user: UserModel;
 }
 
 export default function Navigation(props: UserProps): JSX.Element {
-  const { user } = props;
-
   const auth = useAuth();
-
-  const [opened, { toggle, close }] = useDisclosure();
-
   const onLogoutHandler = auth.logout;
+  const { t } = useTranslation();
+
+  const userNavigation = [
+    {
+      name: t('navigation.signOut'),
+      onClick: onLogoutHandler,
+      color: 'red',
+      icon: IconLogout2
+    }
+  ];
+
+  const { user } = props;
 
   return (
     <nav className="border-b border-primary-300/25 bg-primary-800 lg:border-none">
-      <div className="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8">
+      <div className="mx-auto px-2 sm:px-4 lg:px-8">
         <div className="relative flex h-16 items-center justify-between lg:border-b lg:border-primary-400/25">
           <div className="flex items-center px-2 lg:px-0">
             <div className="shrink-0">
@@ -65,23 +45,6 @@ export default function Navigation(props: UserProps): JSX.Element {
               </div>
             </div>
           </div>
-          <div className="flex lg:hidden">
-            {/* Mobile menu button */}
-            <div className="group relative inline-flex items-center justify-center rounded-md bg-primary-600 p-2 text-primary-200 hover:bg-primary-500/75 hover:text-white focus:outline-hidden focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-primary-600">
-              <Burger
-                aria-label="Toggle navigation"
-                opened={opened}
-                size="sm"
-                classNames={{
-                  burger: 'bg-primary-200 before:bg-primary-200 after:bg-primary-200'
-                }}
-                onClick={toggle}
-              >
-                <span className="absolute -inset-0.5" />
-                <span className="sr-only">Open main menu</span>
-              </Burger>
-            </div>
-          </div>
 
           <div className="hidden lg:ml-4 lg:block">
             <div className="flex items-center">
@@ -92,69 +55,28 @@ export default function Navigation(props: UserProps): JSX.Element {
                     type="button"
                   >
                     <span className="absolute -inset-1.5" />
-                    <span className="sr-only">Open user menu</span>
-                    <img alt="" className="size-8 rounded-full" src={user.imageUrl} />
+                    <span className="sr-only">{t('navigation.openMainMenu')}</span>
+                    <IconUserFilled className="size-8 rounded-full border-primary-500 border-1 p-0.5" />
                   </button>
                 </Menu.Target>
 
                 <Menu.Dropdown>
-                  <Menu.Item
-                    color="red"
-                    leftSection={<IconLogout2 style={{ width: rem(14), height: rem(14) }} />}
-                    onClick={onLogoutHandler}
-                  >
-                    Logout
-                  </Menu.Item>
+                  {userNavigation.map(({ name, onClick, color, icon: Icon }) => (
+                    <Menu.Item color={color} leftSection={<Icon style={{ width: 14, height: 14 }} />} onClick={onClick}>
+                      {name}
+                    </Menu.Item>
+                  ))}
                 </Menu.Dropdown>
               </Menu>
             </div>
           </div>
+
+          <div className="flex lg:hidden">
+            {/* Mobile menu */}
+            <NavigationDrawer user={user} userNavigation={userNavigation} />
+          </div>
         </div>
       </div>
-
-      <Drawer
-        opened={opened}
-        size="xs"
-        classNames={{
-          content: 'bg-primary-600',
-          header: 'bg-primary-600',
-          close: 'text-primary-200'
-        }}
-        onClose={toggle}
-      >
-        <div className="lg:hidden">
-          <div className="space-y-1 px-2 pb-3 pt-2">
-            <nav onClick={close}>
-              <NavigationMenu
-                activeClassName="bg-primary-700 text-white"
-                className="block rounded-md px-3 py-2 text-base font-medium text-white hover:bg-primary-500/75"
-              />
-            </nav>
-          </div>
-          <div className="border-t border-primary-700 pb-3 pt-4">
-            <div className="flex items-center px-5">
-              <div className="shrink-0">
-                <img alt="" className="size-10 rounded-full" src={user.imageUrl} />
-              </div>
-              <div className="ml-3">
-                <div className="text-base font-medium text-white">{user.name}</div>
-                <div className="text-sm font-medium text-primary-300">{user.email}</div>
-              </div>
-            </div>
-            <div className="mt-3 space-y-1 px-2">
-              {userNavigation.map((item) => (
-                <a
-                  key={item.name}
-                  className="block rounded-md px-3 py-2 text-base font-medium text-white hover:bg-primary-500/75"
-                  href={item.href}
-                >
-                  {item.name}
-                </a>
-              ))}
-            </div>
-          </div>
-        </div>
-      </Drawer>
     </nav>
   );
 }
