13d7f7d5f265cc14f1d07c367f4e68bfacaa00ac
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-08-28 17:27:59 +0200

    tweak/style header

--- src/components/ui/Navigation/Navigation.tsx
+++ src/components/ui/Navigation/Navigation.tsx
@@ -30,20 +30,23 @@ export default function Navigation(props: UserProps): JSX.Element {
   const { user } = props;
 
   return (
-    <nav className="border-b border-primary-300/25 bg-primary-800 lg:border-none">
+    <nav className="border-b border-secondary-500/25 bg-secondary-500 lg:border-none shadow-md py-5">
       <div className="mx-auto px-2 sm:px-4 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between lg:border-b lg:border-primary-400/25">
+        <div className="relative flex h-16 items-stretch justify-between lg:border-b lg:border-primary-400/25">
           <div className="flex items-center px-2 lg:px-0">
             <div className="shrink-0">
               <Link to="/dashboard">
-                <NotifycalIsologo className="text-primary-500 h-10" />
+                <NotifycalIsologo
+                  className="text-primary-500 h-10 transition-all duration-300 hover:scale-105 drop-shadow-md hover:drop-shadow-xl drop-shadow-primary-900 hover:drop-shadow-primary-900"
+                  style={{ height: 55 }}
+                />
               </Link>
             </div>
             <div className="hidden lg:ml-10 lg:block">
               <div className="flex space-x-4">
                 <NavigationMenu
-                  activeClassName="bg-primary-900"
-                  className="rounded-md px-3 py-2 text-sm font-medium text-white hover:bg-primary-500/75"
+                  activeClassName="inset-shadow-[0_2px_4px_var(--color-primary-900)] bg-secondary-600 hover:inset-shadow-none"
+                  className="block rounded-md px-3 py-2 text-md font-medium text-white hover:bg-accent1-500/75 hover:text-accent1-800 hover:no-underline transition-all duration-300 hover:scale-105 hover:shadow-md hover:shadow-primary-900"
                 />
               </div>
             </div>
@@ -55,12 +58,16 @@ export default function Navigation(props: UserProps): JSX.Element {
                 <Menu position="bottom-end" shadow="md" width={200}>
                   <Menu.Target>
                     <button
-                      className="flex items-center gap-2 px-4 py-2 hover:hover:bg-primary-500/75 rounded-xl"
+                      className="group flex items-center justify-center gap-2 px-4 py-2 hover:bg-accent1-500/75 rounded-xl transition-all duration-300 hover:scale-105 hover:shadow-md hover:shadow-primary-900 hover:border-accent1-300"
                       type="button"
                     >
                       <div className="text-right">
-                        <div className="text-sm font-medium text-primary-200">{user.config?.business.name}</div>
-                        <div className="text-xs font-medium text-primary-50">{user.email}</div>
+                        <div className="text-sm font-medium text-primary-200 group-hover:text-accent1-900">
+                          {user.config?.business.name}
+                        </div>
+                        <div className="text-xs font-medium text-primary-50 group-hover:text-accent1-800">
+                          {user.email}
+                        </div>
                       </div>
                       <div className="size-8 relative flex rounded-full bg-primary-600 text-sm text-white focus:outline-hidden focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-primary-600">
                         <span className="absolute -inset-1.5" />
