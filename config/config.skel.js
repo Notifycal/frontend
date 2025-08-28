33b465b5044961080ddd015a424ad09a40eadfac
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-08-28 03:48:46 +0200

    use real sms cost in billing page banner

--- config/config.skel.js
+++ config/config.skel.js
@@ -2,5 +2,6 @@ window.globalConfig = {
   GOOGLE_CLIENT_ID: ${googleClientId},
   BACKEND_BASE_URL: ${backendBaseUrl},
   STATIC_LANDING_URL: ${staticLandingUrl},
-  TIER_INFO: ${tierInfo}
+  TIER_INFO: ${tierInfo},
+  COUNTRY_TO_SMS_COST_MAP: ${countryToSmsCostMap}
 };
