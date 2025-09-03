831ece79c89a5b3f2e41acae296bd1f913cdc18c
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-09-03 20:27:48 +0200

    don't gop to welcome if editting profil;er

--- src/routes/_auth/onboarding/index.tsx
+++ src/routes/_auth/onboarding/index.tsx
@@ -24,9 +24,8 @@ export const Route = createFileRoute('/_auth/onboarding/')({
     }
 
     const { completedSteps } = useOnboardingStore.getState();
-
     const firstIncompleteIndex = getFirstIncompleteStepIndex(completedSteps) || 0;
-    if (firstIncompleteIndex === 0) {
+    if (!search.edit && firstIncompleteIndex === 0) {
       throw redirect({ to: '/onboarding/welcome' });
     }
     const path = getStepByIndex(firstIncompleteIndex)?.path || '';
