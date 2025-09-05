1f014dc22b28f5c604d95a94d108f5561cb1c5f1
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-09-05 14:52:22 +0200

    fix: reduce google oauth scopes

--- src/auth/google.ts
+++ src/auth/google.ts
@@ -6,12 +6,8 @@ const GOOGLE_SCOPE_BASE_PREFIX = 'https://www.googleapis.com/auth';
 const GOOGLE_OAUTH_SCOPE_SUFFIXES = [
   'userinfo.profile',
   'userinfo.email',
-  'calendar',
   'calendar.readonly',
-  'calendar.events',
   'calendar.events.readonly',
-  'calendar.settings.readonly',
-  'calendar.addons.execute',
   'contacts.readonly'
 ];
 
