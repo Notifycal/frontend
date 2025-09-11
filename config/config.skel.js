970b57a6c2a6e0559550b0209cad8ee303c65f54
Daniel Siguero <26867647+dsiguero@users.noreply.github.com>
2025-09-11 22:13:07 +0200

    feat: set GTM script in index.html for early loading (#686)

--- config/config.skel.js
+++ config/config.skel.js
@@ -3,5 +3,6 @@ window.globalConfig = {
   BACKEND_BASE_URL: ${backendBaseUrl},
   STATIC_LANDING_URL: ${staticLandingUrl},
   TIER_INFO: ${tierInfo},
-  COUNTRY_TO_SMS_COST_MAP: ${countryToSmsCostMap}
+  COUNTRY_TO_SMS_COST_MAP: ${countryToSmsCostMap},
+  GOOGLE_TAG_MANAGER_ID: ${googleTagManagerId}
 };
