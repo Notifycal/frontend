62538ca5a3d264c6b808ea1cad8e0df462c48115
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-09-24 14:32:42 +0200

    bump shared

--- config/config.local.js
+++ config/config.local.js
@@ -1,6 +1,6 @@
 window.globalConfig = {
   GOOGLE_CLIENT_ID: '658640078137-omuaokg6rcajv50879674moielbpvljl.apps.googleusercontent.com',
-  BACKEND_BASE_URL: 'https://6nesmoza3k.execute-api.eu-west-1.amazonaws.com/dev',
+  BACKEND_BASE_URL: 'https://3gkgmufxag.execute-api.eu-west-1.amazonaws.com/dev',
   STATIC_LANDING_URL: 'http://localhost:4321',
   COUNTRY_TO_SMS_COST_MAP: '{ "ES": 5 }',
   TIER_INFO: '{"tiers":{"best":{"credits":6000,"name":"Best Plan","number_of_reminders":1200,"price_eur":99,"price_id":"best_price_id_mock","product_id":"best_product_id_mock"},"better":{"credits":1500,"name":"Better Plan","number_of_reminders":300,"price_eur":44,"price_id":"better_price_id_mock","product_id":"better_product_id_mock"},"good":{"credits":400,"name":"Good Plan","number_of_reminders":80,"price_eur":16,"price_id":"good_price_id_mock","product_id":"good_product_id_mock"}},"topups":{"single":{"credits":500,"name":"Single Topup","number_of_reminders":100,"price_eur":20,"price_id":"topup_single_price_id_mock","product_id":"topup_single_product_id_mock"}}}',
